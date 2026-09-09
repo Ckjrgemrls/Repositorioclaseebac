302036137394bc7010921db858ee94044a11fcc2
Ckjrgemrls <jorgemorales6421@gmail.com>
2026-09-09 14:23:46 -0300

    Hola Mundo
    -Se agrega el Script de "Hola Mundo" a la Main Camera.
    -Se utilizaron las funciones de Debug para imprimir los diferentes tipos de mensajes.

---
+++ El primer Poryecto/Assets/Scenes/Scripts/Holamundo.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Holamundo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        print("Hola Mundo");
+        Debug.Log("Algo está pasando");
+        Debug.LogWarning("Algo podría estar mal");
+        Debug.LogError("Algo está mal");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
